cb6079f73a9009e8ae7ca5650e065f1a68abbc33
Serg1853 <129977140+Serg1853@users.noreply.github.com>
2023-08-26 14:08:29 +0300

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,22 +4,48 @@ const form = document.querySelector('.feedback-form');
 const KEY_FORM_INPUT = 'feedback-form-state';
 
 form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onInputData,500));
+form.addEventListener('input', throttle(onInputData, 500));
+// console.dir(form);
+// console.dir(form.elements.email.value);
 
+shouData();
+
+const onFormData = {};
 function onInputData(evt) {
-    let data = localStorage.getItem(KEY_FORM_INPUT);
-    data = data ? JSON.parse(data) : {};
-    let { email, message } = form.elements;
-    data = {
-        email: email.value.trim(),
-        message: message.value.trim(),
-    }
-    data[evt.target.name] = evt.target.value.trim();
-    localStorage.setItem(KEY_FORM_INPUT, JSON.stringify(data));
+  onFormData[evt.target.name] = evt.target.value;
+  localStorage.setItem(KEY_FORM_INPUT, JSON.stringify(onFormData));
+  //  console.log(evt.target);
+  // const {
+  //   elements: { email, message },
+  // } = form;
+
+  // console.log(onFormData);
+  //  const dataForm = { email: email.value, message: message.value };
+}
+
+function shouData() {
+  let dataForm = JSON.parse(localStorage.getItem(KEY_FORM_INPUT));
+  if (dataForm) {
+    const {
+      elements: { email, message },
+    } = form;
+    email.value = dataForm.email;
+    message.value = dataForm.message;
+  }
 }
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  
-}
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
 
+  if (email.value.trim() === '' || message.value.trim() === '') {
+    return alert('Заповніть всі поля');
+  }
+  // const outputData = { email: email.value.trim(), message: message.value.trim() };
+  console.log({ email: email.value.trim(), message: message.value.trim() });
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(KEY_FORM_INPUT);
+}
